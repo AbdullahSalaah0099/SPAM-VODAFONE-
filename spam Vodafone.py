f2b4065e412cf1cf53100283d4e373c13db04875
Abdullah Salah <95449686+AbdullahSalaah0099@users.noreply.github.com>
2022-05-21 10:23:31 +0200

    Update spam Vodafone.py

--- spam Vodafone.py
+++ spam Vodafone.py
@@ -8,14 +8,14 @@ time.sleep (0)
 print ('\033[1;92mclick on this link to get password👇')
 #sleep (0.1)
 print ()
-link1="\033[1;93m https://miklpro.com/sbpfknb"
+link1="\033[1;93m https://miklpro.com/JICCMdQu"
 print (link1)
 #sleep (1)
 print ()
 password=input ('\033[1;92m》Enter Password Script :  \033[1;96m')
 sleep (1)
 
-rrr=requests.get('https://pastelink.net/mklk8i4b').text
+rrr=requests.get('https://pastelink.net/66dem5ly').text
 soup=BS(rrr,'html.parser')
 lxc=(soup.find('div',{'class':'body-display'})).text
 
